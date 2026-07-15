cf99fec59473788719cbc44846260dcd9a324442
Abdou Karime SENE <connecticlab@gmail.com>
2026-07-15 20:11:13 +0000

    Interface web minimale: login + tableau de bord caissier, teste dans le navigateur avec succes

--- caisse/views.py
+++ caisse/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import CaisseSession, Ticket
 
-# Create your views here.
+
+def tableau_bord_caissier(request):
+    session = CaisseSession.objects.filter(
+        caissier=request.user, statut='ouverte'
+    ).first()
+
+    tickets = []
+    if session:
+        tickets = session.tickets.order_by('-date')[:10]
+
+    context = {
+        'session': session,
+        'tickets': tickets,
+    }
+    return render(request, 'caisse/tableau_bord.html', context)
